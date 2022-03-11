a33cfa5715100e2b61118c9e11f1139834c0ec86
DaniilNekrasov <daniilnekrasov02@gmail.com>
2022-03-10 23:05:46 -0800

    ..

--- DNekrasov_051003/1.sh
+++ DNekrasov_051003/1.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-i=1
+if [ $# -ne 3 ]
+then
+	echo Not enough arguments
+	
+else
 
 find $name -type f -size +$1c -size -$2c -printf '%p %s\n' >$3
 
 find $name -type f -size +$1c -size -$2c -printf '%p %s\n' | wc -l
+
+fi
